cc613abc921a4db8fce75e69676001870abe29cf
ayush-lal <ayush.lal@outlook.com>
2022-09-02 12:19:02 +1000

    getting port from .env

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
+import { port } from "./configs/config";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(8000, () => {
-  console.log("The application is listening on port 8000!");
+app.listen(port, () => {
+  console.log(`The application is listening on port ${port}!`);
 });
