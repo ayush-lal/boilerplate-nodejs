914dde6225057e0100eff149097e70d7d2bc2f6d
ayush-lal <ayush.lal@outlook.com>
2022-09-03 23:57:05 +1000

    added swagger documentation

---
+++ src/utils/swagger/index.ts
@@ -0,0 +1,43 @@
+import { Express, Request, Response } from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Boilerplate NodeJS API",
+      version: "1.0.0",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function swaggerDocs(app: Express, port: string) {
+  // swagger page
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // docs in json format
+  app.get("/docs-json", (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+}
+
+export default swaggerDocs;
